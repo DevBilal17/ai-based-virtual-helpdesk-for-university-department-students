4b7c5b8b4e7ccd8cf9d4d7c5817fd899388a4467
DevBilal17 <muhammadbilalmb03122@gmail.com>
2026-02-27 06:29:34 +0500

    Login Screen UI Done

--- frontend/app/login.jsx
+++ frontend/app/login.jsx
@@ -1,8 +1,10 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ImageBackground, StatusBar, StyleSheet } from 'react-native'
 import React from 'react'
 import { removeItem } from '../utils/asyncStorage'
 import { useNavigation, useRouter } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import GlassmorphismCard from '../components/GlassmorphismCard/GlassmorphismCard'
+import LoginForm from '../components/Forms/LoginForm'
 
 const login = () => {
   const navigation = useNavigation();
@@ -12,11 +14,50 @@ const login = () => {
   };
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <TouchableOpacity onPress={handleReset}>
-        <Text>Reset</Text>
-      </TouchableOpacity>
+       <ImageBackground source={require("../assets/images/on-boarding-bg-1.png")} style={[styles.container,{ flex: 1, justifyContent: 'center', alignItems: 'center' }]}>
+         <StatusBar barStyle="light-content" />
+          <View style={{marginBottom:40}}>
+            <Text style={styles.headTitle}>Hi, Welcome Here!</Text>
+            <Text style={styles.headSubtitle}>Please enter your email adress and password.</Text>
+          </View>
+          <GlassmorphismCard>
+            <Text style={styles.loginText}>Login</Text>
+            <LoginForm/>
+          </GlassmorphismCard>
+       </ImageBackground>
     </SafeAreaView>
   )
 }
 
 export default login
+
+
+const styles = StyleSheet.create({
+  container : {
+     paddingHorizontal:20,
+  },
+  headTitle : {
+    fontSize:40,
+    fontWeight:"bold",
+    fontFamily:"Roboto",
+    color:"#fff",
+    textAlign:"center",
+    lineHeight : 50
+  },
+  headSubtitle : {
+    fontSize:16,
+    fontFamily:"Roboto",
+    color:"#C8CACD",
+    textAlign:"center",
+    lineHeight : 50
+  },
+
+  loginText:{
+    fontFamily:"Roboto",
+    fontSize:24,
+    fontWeight:"bold",
+    color:"#F7FEFF",
+    textAlign:"center",
+  }
+
+})
---
+++ frontend/components/Forms/LoginForm.jsx
@@ -0,0 +1,200 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Pressable,
+} from "react-native";
+import { useForm, Controller } from "react-hook-form";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function LoginForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Form Data:", data);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+          {/* EMAIL */}
+        <Text style={styles.label}>Email</Text>
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: "Email is required",
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: "Enter a valid email",
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  paddingHorizontal:15
+                }
+              ]}
+              placeholder="Enter your email"
+              placeholderTextColor={"#F7FEFF99"}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.email && (
+          <Text style={styles.error}>{errors.email.message}</Text>
+        )}
+      </View>
+
+    
+      <View>
+            {/* PASSWORD */}
+        <Text style={styles.label}>Password</Text>
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: "Password is required",
+            minLength: {
+              value: 6,
+              message: "Minimum 6 characters",
+            },
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  paddingHorizontal:15
+                }
+              ]}
+              placeholder="Enter your password"
+              placeholderTextColor={"#F7FEFF99"}
+              secureTextEntry
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+        {errors.password && (
+          <Text style={styles.error}>{errors.password.message}</Text>
+        )}
+
+        <View style={styles.forgotContainer}>
+        {/* REMEMBER ME */}
+        <Controller
+          control={control}
+          name="remember"
+          render={({ field: { value, onChange } }) => (
+            <Pressable
+              style={styles.rememberContainer}
+              onPress={() => onChange(!value)}
+            >
+              <View style={styles.circle}>
+                {value && <View style={styles.checkedCircle} />}
+              </View>
+              <Text style={styles.rememberLabel}>Remember Me</Text>
+            </Pressable>
+          )}
+        />
+
+        <TouchableOpacity>
+          <Text style={styles.forgotText}>
+            Forgot Password?
+          </Text>
+        </TouchableOpacity>
+
+      </View>
+
+      </View>
+    
+      <TouchableOpacity
+        
+        onPress={handleSubmit(onSubmit)}
+      >
+        <LinearGradient style={styles.button} colors={["#3659F4","#3C82F2"]}>
+            <Text style={styles.buttonText}>Login</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        display:"flex",
+        gap:20,
+        marginTop:25
+    },
+  label: {
+    fontSize: 17,
+    fontFamily:"Inter",
+    marginBottom: 8,
+    fontWeight: "600",
+    color:"#ffffff",
+    marginLeft:15
+  },
+  input: {
+    borderRadius:20,
+    paddingVertical:13,
+    paddingHorizontal:8,
+    backgroundColor:"rgba(247, 254, 255, 0.1)",
+    fontFamily:"Roboto",
+    fontSize:15,
+    fontWeight:"medium",
+    color : "rgba(247, 254, 255, 0.8)"
+  },
+  inputError: {
+    borderColor: "red",
+  },
+  error: {
+    color: "red",
+    marginBottom: 15,
+    fontSize: 12,
+  },
+    rememberContainer: { flexDirection: "row", alignItems: "center", justifyContent:"center" ,marginVertical: 12 },
+  circle: {
+    width: 12, height: 12, borderRadius: 50 + "%",
+    backgroundColor: "#fff",
+    justifyContent: "center", alignItems: "center", marginRight: 10,
+  },
+  checkedCircle: { width: 10, height: 10, borderRadius: 50 + "%", backgroundColor: "#3659F4" },
+  rememberLabel: { fontSize: 12, color: "#fff",fontFamily:"Roboto" },
+
+  forgotContainer:{
+    display:"flex",
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between",
+    gap:10
+  },
+  forgotText:{
+    fontSize: 12, color: "#fff",fontFamily:"Poppins" 
+  },
+  button: {
+    borderRadius:50,
+    padding:13,
+    marginTop:10
+  },
+  buttonText: {
+     fontSize:20,
+     fontWeight:"bold",
+     fontFamily:"Roboto",
+     color:"#fff",
+     textAlign:"center"
+  },
+});
