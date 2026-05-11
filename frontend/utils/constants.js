ec7899f55c08e3fe5955caf8f0d9e87b7b55686e
Abdul Waleed <abdulwaleedfsd@gmail.com>
2026-05-11 11:57:46 -0700

    Resolved some conflict problems after merge main branch conflict

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,24 +1,20 @@
+// export const BASE_URL = "http://172.19.48.129:5000"
 
-<<<<<<< HEAD
-export const BASE_URL = "http://172.19.48.129:5000"
+// export const BASE_URL_8000 = "http://172.19.48.129:8000"
 
+export const BASE_URL = "http://10.133.187.123:5000";
 
-export const BASE_URL_8000 = "http://172.19.48.129:8000"
-=======
-export const BASE_URL = "http://10.133.187.123:5000"
-
-
-export const BASE_URL_8000 = "http://10.133.187.123:8000"
->>>>>>> 1a7f37397712dc4acb98b894a51a1e7f2e3ecc1d
+export const BASE_URL_8000 = "http://10.133.187.123:8000";
+// >>>>>>> 1a7f37397712dc4acb98b894a51a1e7f2e3ecc1d
 
 export const COLORS = {
-  primary: "#635BFF",      // The vibrant purple from your "Add User" button
-  background: "#0C1013",   // Deep navy/black background
-  surface: "#1C2D47",      // Card/Input background
-  accentBlue: "#3B82F6",   // From the "Student" badge
-  success: "#10B981",      // From the "Active" status
-  error: "#EF4444",        // From the delete icon
+  primary: "#635BFF", // The vibrant purple from your "Add User" button
+  background: "#0C1013", // Deep navy/black background
+  surface: "#1C2D47", // Card/Input background
+  accentBlue: "#3B82F6", // From the "Student" badge
+  success: "#10B981", // From the "Active" status
+  error: "#EF4444", // From the delete icon
   textMain: "#FFFFFF",
-  textMuted: "#8E9AAF",    // Secondary gray text
+  textMuted: "#8E9AAF", // Secondary gray text
   glassBorder: "rgba(255, 255, 255, 0.1)",
 };
